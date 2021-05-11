b63c81f8890a53c506618bd0cb900c8ddd11fedb
SamMoudry <73550040+SamMoudry@users.noreply.github.com>
2021-05-11 11:33:14 -0500

    Update joins-solution.sql

--- joins-solution.sql
+++ joins-solution.sql
@@ -3,21 +3,31 @@ SELECT * FROM customers
 JOIN addresses ON customers.id = addresses.customer_id;
 
 -- 2. Get all orders and their line items (orders, quantity and product).
+SELECT * FROM orders
+JOIN line_items ON orders.id = line_items.order_id;
 
+-- 3. Which warehouses have cheetos? -delta
+SELECT * FROM warehouse
+JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id
+WHERE warehouse_product.product_id = 5;
 
--- 3. Which warehouses have cheetos?
+-- 4. Which warehouses have diet pepsi? -alpha, delta, gamma
+SELECT * FROM warehouse
+JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id
+WHERE warehouse_product.product_id = 6;
 
+-- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results. -3, 1, 5
+SELECT COUNT(*) FROM orders
+JOIN addresses ON orders.address_id = addresses.id
+JOIN customers ON addresses.customer_id = customers.id
+GROUP BY customers.id;
 
--- 4. Which warehouses have diet pepsi?
+-- 6. How many customers do we have? -4
+SELECT COUNT(*) FROM customers;
 
+-- 7. How many products do we carry? -7
+SELECT COUNT(*) FROM products;
 
--- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
-
-
--- 6. How many customers do we have?
-
-
--- 7. How many products do we carry?
-
-
--- 8. What is the total availabel on-hand quantity of diet pepsi?
+-- 8. What is the total available on-hand quantity of diet pepsi? -92
+SELECT SUM(on_hand) FROM warehouse_product
+WHERE warehouse_product.product_id = 6;
